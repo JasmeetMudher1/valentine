e2e70cec32db4fd6844fb4517321b2cc2718eb17
Jasmeet Singh Mudher <110900189+JasmeetMudher@users.noreply.github.com>
2025-02-04 18:12:00 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    basePath: '/valentine', // Replace with your repo name
-    assetPrefix: '/valentine/',
-  };
 export default nextConfig;
