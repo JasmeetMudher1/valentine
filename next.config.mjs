054227871e8e2db0f57bb0b4e7c3e055d0dd86e8
Jasmeet Singh Mudher <110900189+JasmeetMudher@users.noreply.github.com>
2025-02-04 18:18:28 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // If you're deploying to a subdirectory like "your-repo-name"
+    basePath: '/valentine',  // Replace with your actual repo name
+};
 
 export default nextConfig;
